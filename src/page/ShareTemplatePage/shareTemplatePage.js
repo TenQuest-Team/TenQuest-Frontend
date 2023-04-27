ac78eccaef3e98c0b79305b2adfe4cc5f4c818ac
garden <gkdlqkql6@naver.com>
2023-04-28 01:11:03 +0900

    질문지 공유하기 페이지 css 구현

--- src/page/ShareTemplatePage/shareTemplatePage.js
+++ src/page/ShareTemplatePage/shareTemplatePage.js
@@ -2,16 +2,17 @@ import { push } from "../../router.js";
 
 export default function ShareTemplatePage({$target}) {
   const $body = document.createElement('div');
-    $body.className = 'body';
+    $body.className = 'body-share';
   
   const $creationCompleteDiv = document.createElement('div');
   $creationCompleteDiv.id = 'creationCompleteDiv';
   
   const $shareButton = document.createElement('button');
   $shareButton.innerText = "링크 복사하기";
-
+  $shareButton.className = 'share-btn';
   this.render = () => {
-    const $successMessage = document.createElement('p');
+    const $successMessage = document.createElement('div');
+    $successMessage.className = 'success-Msg'
     $successMessage.innerText = "질문지가 성공적으로 생성되었습니다!"
   
     
